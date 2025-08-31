3b2dcfd2d4f7f2bc625824665c92e75ab37418be
mntone <901816+mntone@users.noreply.github.com>
2025-09-01 07:09:29 +0900

    feat: rp2350 support

---
+++ cmake/toolchains/pico-rp2350-gcc.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Pico)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+
+if(PICO_TOOLCHAIN)
+	set(EMBED_TEST_TOOLCHAIN_PREFIX "${PICO_TOOLCHAIN}" CACHE INTERNAL "")
+else()
+	set(EMBED_TEST_TOOLCHAIN_PREFIX "arm-none-eabi" CACHE INTERNAL "")
+endif()
+set(EMBED_TEST_GCC_CPU_FLAGS "-mcpu=cortex-m33 -mthumb -mno-unaligned-access -march=armv8-m.main+fp+dsp -mfloat-abi=softfp")
+#if (NOT INKNIT_NO_CMSE)
+#	set(EMBED_TEST_GCC_CPU_FLAGS "${CMAKE_C_FLAGS} -mcmse")
+#endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/pico-common-gcc.cmake)
+
+unset(EMBED_TEST_GCC_CPU_FLAGS)
